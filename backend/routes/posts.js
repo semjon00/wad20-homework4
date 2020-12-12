e8dd91014e3e85c5bb85af3b2a40b1137abc31a3
Semjon Kravtšenko <semjon.00@gmail.com>
2020-12-12 12:59:42 +0200

    Tasks 2 and 3 DONE

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -32,12 +32,29 @@ router.post('/', authorize,  (request, response) => {
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+
+    // Important: SECURITY
+    // If getLikesByUserIdAndPostId check is removed,
+    // powerusers may create unbounded number of likes for any given post by sending repeating API requests!
+    PostModel.getLikesByUserIdAndPostId(request.currentUser, request.params.postId, (likes) => {
+        if (likes.length !== 0) {
+            response.status(500).json({}); // Better message?
+            return;
+        }
+
+        PostModel.like(request.currentUser, request.params.postId, () => {
+            response.status(201).json({});
+        });
+    })
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
 
+    PostModel.unlike(request.currentUser, request.params.postId, () => {
+        response.status(201).json({});
+    });
 });
 
 module.exports = router;
